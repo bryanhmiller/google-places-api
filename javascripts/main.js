d113e60b74705538c02e44388796c10c601a1cbb
bryanhmiller <bryankalos@hotmail.com>
2017-04-24 21:32:26 -0500

    Code along for api

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,25 @@
-console.log("Javascript loading");
+$(document).ready(function(){
+
+	const apiKey = "";
+
+	$("body").on("click", "li", (e) => {
+	// console.log(e.target.innerHTML);
+		loadPlaces(e.target.innerHTML).then((data) => {
+			console.log(data);
+		}).catch((error) => {
+			console.log(error);
+		});
+	});
+
+	const loadPlaces = (dropdownType) => {
+		return new Promise ((resolve, reject) => 
+			$.ajax(`https://maps.googleapi0s.com/maps/api/place/nearbysearch/json?location=36.174465,-86.7767960&rankby=distance&type=${dropdownType}&key=${apiKey}`)
+			).done((data) => resolve(data)
+			).fail((error) => reject(error));
+		};
+	};
+
+
+
+
+});
